99174b1202392d4c464f8e73d3a4b0392839493c
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-06-16 12:23:33 -0300

    Revert "Revert "v0.18.06.12""
    This reverts commit 5b74853183cde5e7393daba96672e0983d3bfbee.

--- application/views/header/menu_top.php
+++ application/views/header/menu_top.php
@@ -1,49 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<style>
-    body {
-        margin-top: 200px;
-    }
-    /****************** NAV BAR ***************/
-    .navbar {
-        box-shadow: 0 4px 20px -4px #ababab;
-        background: rgba(249, 249, 249, .8);
-        transition: background .8s ease;
-        z-index: 10;
-    }
 
-    .navbar:hover {
-        background: #C0D6FF;
-    }
-    /**************** LARGE */
-    .fsz-lg {
-        font-size: 50px;
-    }
-    .logo-lg {
-        height: 100px;
-    }
-    /****************** SMALL *********/
-    .fsz {
-        font-size: 40px;
-    }
-    .logo {
-        height: 40px;
-        width: 200px;
-    }
-
-    @media (max-width: 1024px) {
-        .col-lg-0 {
-            display: none;
-        }
-        .btn-toggle {
-            display: block;
-        }
-    }
-</style>
 <header>
-	<nav class="navbar fixed-top navbar-expand-lg">
-		<a href="<?php echo base_url(PATH); ?>"><img src="<?php echo base_url('img/logo/logo-brapci.png'); ?>" id="logo" class="logo-lg col-lg-0" border=0>
+	<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+		MOOC
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -55,6 +16,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<li class="nav-item">
 					<a class="nav-link fsz " href="<?php echo base_url(PATH . 'about'); ?>">&nbsp;<?php echo msg('about'); ?>&nbsp;</a>
 				</li>
+				<li class="nav-item">
+					<a class="nav-link fsz " href="<?php echo base_url(PATH . 'courses'); ?>">&nbsp;<?php echo msg('courses'); ?>&nbsp;</a>
+				</li>				
 				<li class="nav-item dropdown">
 					<a class="nav-link fsz dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">&nbsp;<?php echo msg('tools'); ?>&nbsp;</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
