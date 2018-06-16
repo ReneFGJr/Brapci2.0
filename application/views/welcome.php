34a5dc8ea614ae0a9dc94b5e37abd22965772384
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-06-16 12:26:34 -0300

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "v0.18.06.12"""""""
    This reverts commit 658c66af109b8bddaa880062e99023ee4160b43f.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,11 +1,3 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<style>
-	body
-		{
-			background: url('img/background/1.jpg');
-			background-size: 100%;
-		}
-</style>
-<body></body>
