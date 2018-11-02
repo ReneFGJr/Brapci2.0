900ad82e8619f3681067bc2815d120420ed2016d
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-11-02 11:23:18 -0300

    v0.18.11.02

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -85,7 +85,17 @@ if (isset($meta))
         google_ad_client: "ca-pub-4492401449039504",
         enable_page_level_ads: true
       });
-    </script>    
+    </script> 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-12713129-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+       
 <!---    
 <script>
 	(function(i, s, o, g, r, a, m) {
