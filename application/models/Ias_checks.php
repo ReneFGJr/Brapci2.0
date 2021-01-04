8ff3c804e6af1f49899f7a18a44db429fd26dece
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-01-04 20:11:42 -0300

    V0.21.01.04

--- application/models/Ias_checks.php
+++ application/models/Ias_checks.php
@@ -25,40 +25,110 @@ class ias_checks extends CI_Model
                 $line = $rlt[$r];
                 $idl = $line['d_literal'];
 
-                $sql = "SELECT * FROM rdf_data where d_literal = $idl";
+                $sql = "SELECT * FROM rdf_data 
+                            where d_literal = $idl";
                 $rrr = $this->db->query($sql);
                 $rrr = $rrr->result_array();
                 $max = 0;
                 $fl = array();
                 $sx = '';
+
+                $n = array();
+                $n[3] = 0;
+                $n[4] = 0;
+
                 for ($y=0;$y < count($rrr);$y++)
                     {
                         $ln = $rrr[$y];
                         $fx = 'c/'.$ln['d_r1'].'/name.ABNT';
+                        echo '<br>'.$fx;
                         if (file_exists($fx))
                         {
-                            $fl[$y] = file_get_contents($fx);
+                            array_push($fl,file_get_contents($fx));
+                            $n[3] =  $n[3] + 1;
+                            echo ' OK';
+                        } else {
+                            echo ' ERRO';
                         }
 
                         $file = 'c/'.$ln['d_r1'].'/name.nm';
                         if (file_exists($file))
                             {
                                 $t = file_get_contents($file);
-                                $sx .= $t;
+                                $sx .= $t.'<hr>';
                             }
+
                         if ($ln['d_r1'] > $max)
                             {
                                 $max = $ln['d_r1'];
                             }                            
                     }
-                //$rdf->remove_concept($max);
-
-                echo $sx;
+                /********************** Arquivo não existe */
+                echo '<h2>Fase I</h2>';
                 echo '<pre>';
-                print_r($fl);
+                print_r($n);
+                echo 'Arquivos: '.count($fl);
                 echo '</pre>';
-                echo '<meta http-equiv="refresh" content="30">';
+                if (($n[3] > 0) and (count($fl) > 0))
+                {
+                    /********************** Anos da publicação */
+                    $v1 = $this->recupera_dados($fl[0],'PY');
+                    $v2 = $this->recupera_dados($fl[1],'PY');
+                    $n[0] = ($v1 == $v2);
+
+                    /********************* Primeiro autor */
+                    $v1 = $this->recupera_dados($fl[0],'AU');
+                    $v2 = $this->recupera_dados($fl[1],'AU');
+                    $n[1] = ($v1 == $v2);
+
+                    /********************* Journal ********/
+                    $v1 = $this->recupera_dados($fl[0],'T2');
+                    $v2 = $this->recupera_dados($fl[1],'T2');
+                    $n[2] = ($v1 == $v2);
+
+                    print_r($n);
+                    echo '<hr>';
+                    echo '<pre>'.$fl[0].'</pre>';
+                    if ($this->neuro($n) == 1)
+                        {
+                            $rdf->remove_concept($max);                    
+                        }
+                }
+                echo '<hr>';
+                echo '===>'.$max;
+                echo '<hr>';
+                echo $sx;
+                
+                echo '<meta http-equiv="refresh" content="2">';
                 exit;                 
             }
         }
+
+        function neuro($n)
+            {
+                $rs = 0;
+                print_r($n);
+                if ($n[3] == 1) { return(1); }
+
+                if (($n[0] == 1) and ($n[1] == 1) and ($n[2] == 1))
+                    {
+                        $rs = 1;
+                    }
+                return($rs);
+            }
+
+        function recupera_dados($txt,$tp)
+            {
+                
+                switch($tp)
+                    {
+                        case 'PY':
+                        $t = substr($txt,strpos($txt,$tp.' - ')+5,4);
+                        break;
+
+                        default:
+                        $t = '';
+                    }
+                return($t);
+            }
 }
