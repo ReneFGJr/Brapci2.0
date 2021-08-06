63577fbb6e28873673ae368b11b0b5e202611b49
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-08-06 12:58:50 -0300

    v0.21.08.06

--- application/helpers/form_sisdoc_helper_5.php
+++ application/helpers/form_sisdoc_helper_5.php
@@ -1,4 +1,19 @@
 <?php
+function wclose()
+    {
+        $sx = '<script>	window.opener.location.reload(); close();</script>';
+        return($sx);
+    }
+
+function newxy($url, $xx, $yy) 
+{
+    $sx = "
+    NewWindow = window.open($url, 'newwin', 'scrollbars=yes,resizable=no,width=$xx,height=$yy,top=10,left=10');
+    NewWindow.focus();
+    void (0);
+    ";
+    return($sx);
+}    
 function menus()
     {
 
